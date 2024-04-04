4752d1bca084de4f638329d6a72789ec2961d200
Albert Cheruiyot <bettalbert99@gmail.com>
2024-04-04 10:52:24 +0300

    First commit

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(' ');
+    const capitalWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return capitalWords.join(' ');
+  });
 }
+
+console.log(titleCased());
